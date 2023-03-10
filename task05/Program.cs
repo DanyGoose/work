af9babda30822be2560ef0b0e8da9e14dd81a353
Danila <danila1996F@gmail.com>
2023-03-10 21:05:40 +0300

    completed task05

--- task05/Program.cs
+++ task05/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber > 0)
+{
+    int tmp = userNumber * -1; 
+    while ( tmp <= userNumber )
+    {
+        Console.Write($"{tmp} ");
+        tmp++;
+    }
+}
+else
+{
+    int tmp = userNumber * -1;
+    while ( userNumber <= tmp )
+    {
+        Console.Write($"{userNumber} ");
+        userNumber++;
+    }
+}
