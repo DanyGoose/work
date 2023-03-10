e93588ce616eb6492fe4afd63245e57b66530a4c
Danila <danila1996F@gmail.com>
2023-03-10 21:56:09 +0300

    completed task04

--- task04/Program.cs
+++ task04/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+if (numberOne > numberTwo && numberOne > numberThree)
+{
+    Console.WriteLine($"{numberOne} больше, чем остальные числа");
+}
+else if (numberTwo > numberThree)
+{
+    Console.WriteLine($"{numberTwo} больше, чем остальные числа");
+}
+else
+    Console.WriteLine($"{numberThree} больше, чем остальные числа");
