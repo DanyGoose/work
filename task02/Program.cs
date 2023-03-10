45ff6c7b6167c8d63d41cf4282e448507c096053
Danila <danila1996F@gmail.com>
2023-03-10 21:42:13 +0300

    completed task02

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,4 +1,10 @@
 ﻿Console.WriteLine("Введите первое число: ");
 int numberOne = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
-int numberOne = Convert.ToInt32(Console.ReadLine());
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if (numberOne > numberTwo)
+{
+    Console.WriteLine($"{numberOne} больше, чем {numberTwo}");
+}
+else
+    Console.WriteLine($"{numberTwo} больше, чем {numberOne}");
