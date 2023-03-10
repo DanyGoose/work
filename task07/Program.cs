207f9acf41518bf7559933464a4a5238626ed289
Danila <danila1996F@gmail.com>
2023-03-10 21:23:29 +0300

    completed task07

--- task07/Program.cs
+++ task07/Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.Write("Введите трехзначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if ( userNumber > 99 && userNumber < 1000 )
+{
+    userNumber = userNumber % 10;
+    Console.WriteLine(userNumber);
+}
+else 
+    Console.WriteLine("Вы ввели не трехзначное число!");
