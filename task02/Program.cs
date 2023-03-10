f6fc5a62e2921b2e535e91ac02c33b95fc8fa877
Danila <danila1996F@gmail.com>
2023-03-10 21:36:20 +0300

    added task02

---
+++ task02/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
--- task07/Program.cs
+++ task07/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Write("Введите трехзначное число: ");
 int userNumber = Convert.ToInt32(Console.ReadLine());
-if ( userNumber > 99 && userNumber < 1000 )
+if (userNumber > 99 && userNumber < 1000)
 {
     userNumber = userNumber % 10;
     Console.WriteLine(userNumber);
 }
-else 
+else
     Console.WriteLine("Вы ввели не трехзначное число!");
