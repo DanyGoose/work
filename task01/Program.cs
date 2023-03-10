2ddd1b37a054c5a6b00168409dc1bbed0159cd82
Danila <danila1996F@gmail.com>
2023-03-10 15:50:55 +0300

    completed task 01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое целое число:");
+int numberOne = Convert.ToInt32(Console.WriteLine());
+Console.Write("Введите второе целое число:");
+int numberTwo = Convert.ToInt32(Console.WriteLine());
+int square = numberTwo * numberTwo;
+if (numberOne == square)
+{
+    Console.ReadLine("квадрат второго числа равен первому числу.");
+}
+else 
+{
+    Console.ReadLine("квадрат второго числа не равен первому числу.");
+}
