9890a4c863595c6dcd4192d1a16320edfe9dc038
Danila <danila1996F@gmail.com>
2023-03-10 22:03:10 +0300

    added task06 and completed task06

---
+++ task06/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите целое число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber % 2 == 0)
+{
+    Console.WriteLine("Ваше число четное!");
+}
+else 
+    Console.WriteLine("Ваше число нечетное!");
