af7b10c8fb11306bfcc5ad359c5d3dbc07b2e2b0
Danila <danila1996F@gmail.com>
2023-03-15 17:10:07 +0300

    completed task14

--- task14/Program.cs
+++ task14/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = Multiplication(number, 7, 23);
+Console.WriteLine(result ? "Кратно" : "Не кратно");
+
+bool Multiplication(int firstNumber, int secondNumber, int thirdNumber)
+{
+    return firstNumber % secondNumber == 0 && firstNumber % thirdNumber == 0;
+}
+
+//if (number % 7 == 0 && number % 23 == 0) Console.WriteLine("Кратно");
+//else Console.WriteLine("Не кратно");
