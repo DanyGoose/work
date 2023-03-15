32e3ea961dbd077fed0f7a20ca5f27925fb38c4c
Danila <danila1996F@gmail.com>
2023-03-15 15:54:12 +0300

    completed task12

--- task12/Program.cs
+++ task12/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber % secondNumber == 0)
+    Console.WriteLine("кратно");
+else
+{
+    int tmp = firstNumber % secondNumber;
+    Console.WriteLine($"не кратно, остаток {tmp}");
+}
