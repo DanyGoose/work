7772ed1136e496f778d72fce4734f9b4781411a0
Danila <danila1996F@gmail.com>
2023-03-16 16:43:34 +0300

    completed task 15

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите день недели (от 1 до 7)");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 8 && number > 0)
+{
+    if (number > 5) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}
+else
+    Console.WriteLine("ОТ 1 ДО 7 !!");
