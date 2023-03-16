b576b7628470628b0911c1e14bb94ee83cef7310
Danila <danila1996F@gmail.com>
2023-03-16 16:34:52 +0300

    completed task 13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 || number < -99)
+{
+    if (number < 1000 && number > -1000) 
+    {
+        number = number % 10;
+        Console.WriteLine(number);
+    }
+    else
+    {
+        while(number > 999 || number < -1000)
+        {
+            number = number / 10;
+        }
+        number = number % 10;
+        Console.WriteLine(number);
+    }
+}
+else Console.WriteLine("Третьей цифры нет");
