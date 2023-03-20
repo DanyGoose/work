3319a6d78686c09cbaa2a6b8c2a8887f53193a24
Danila <danila1996F@gmail.com>
2023-03-20 19:54:27 +0300

    completed task 19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9999 && number < 100000) YesNo(number);
+else Console.WriteLine("Число не подходит!");
+
+void YesNo(int num0)
+{
+    int num1 = num0 / 10000;
+    int num2 = num0 % 10;
+    if (num1 == num2)
+    {
+        num1 = num0 / 1000 % 10;
+        num2 = num0 % 100 / 10;
+        if (num1 == num2)  Console.WriteLine("Палиндром");
+    }
+    else Console.WriteLine("Не палиндром");
+} 
