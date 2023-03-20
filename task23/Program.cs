63b47867e61575795caca8435d916cb4bd206133
Danila <danila1996F@gmail.com>
2023-03-20 20:18:07 +0300

    completed 23

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0) Cube(number);
+else Console.WriteLine("НЕ ПОДХОДИТ!");
+
+void Cube(int i)
+{
+    int tmp = 1;
+    while (tmp <= i)
+    {
+        int res = tmp * tmp * tmp;
+        Console.WriteLine($"{res} ");
+        tmp++;
+    }
+}
