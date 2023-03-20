ba1cbad887be24a8debfafdc390b8cce6395e930
Danila <danila1996F@gmail.com>
2023-03-20 18:34:57 +0300

    completed task 18

--- task18/Program.cs
+++ task18/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер четветри (от 1 до 4): ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string result = Quarter(quarter);
+Console.WriteLine(result);
+
+string Quarter(int quart)
+{
+    if(quart == 1) return "x > 0 и y > 0";
+    if(quart == 2) return "x < 0 и y > 0";
+    if(quart == 3) return "x < 0 и y < 0";
+    if(quart == 4) return "x > 0 и y < 0";
+    return "недопустимое значение!";
+}
