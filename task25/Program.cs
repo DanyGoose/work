401c0c695f9459eec436774f7519b6b355701d57
dotnet <danila1996F@gmail.com>
2023-03-23 23:27:28 +0300

    completed task 25

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень, в которую нужно возвести это число: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+if (degree > 0)
+{
+    int res = Exponentiation(number, degree);
+    Console.WriteLine($"{number} в степени {degree} -> {res}");
+}
+else Console.WriteLine("Степень должна быть положительным числом!");
+
+int Exponentiation(int num, int deg)
+{
+    int tmp = 1;
+    for (int i = 0; i < deg; i++)
+    {
+        tmp = tmp * num;
+    }
+    return tmp;
+}
