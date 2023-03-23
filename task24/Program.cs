6aba9169404d4bb597431781d196d10e7db76f51
dotnet <danila1996F@gmail.com>
2023-03-23 23:03:40 +0300

    completed task 24

--- task24/Program.cs
+++ task24/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"сумма чисел от 1 до {number} = {sumNumbers}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; 1 <= num; i++)
+    {
+        sum = sum + 1; 
+    }
+    return sum;
+}
