bea1fd7f227a6ca17f13ca4fa747d2b1d48aba78
dotnet <danila1996F@gmail.com>
2023-03-24 00:18:20 +0300

    completed task 27

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = Remains(number);
+Console.WriteLine(result);
+
+int Remains(int num)
+{
+    int tmp = 0;
+    int res = 0;
+    while (num > 9 || num < -9)
+    {
+        tmp = num % 10;
+        res = res + tmp;
+        num = num / 10;
+    }
+    res = res + num;
+    return res;
+}
