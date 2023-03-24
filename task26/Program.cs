20bfa0f478dcccaad29a9619448982db2caea785
dotnet <danila1996F@gmail.com>
2023-03-24 13:55:30 +0300

    completed task 26

--- task26/Program.cs
+++ task26/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit = Digit(number);
+Console.WriteLine($"Кол-во цифр в числе -> {digit}");
+
+int Digit (int num)
+{
+    int i = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        i++;
+    }
+    return i;
+}
+
