6d35189a896f9dbde3f60120fdd9a8b2ca3ea431
dotnet <danila1996F@gmail.com>
2023-03-24 14:47:36 +0300

    completed task 30

--- task30/Program.cs
+++ task30/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿RandArray();
+
+void RandArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        int number = new Random().Next(0, 2);
+        array[i] = number; 
+        Console.Write(array[i] + " ");
+    }
+}
