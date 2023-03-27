031f25d58a8086f10b2674064ce2a73ef505d653
dotnet <danila1996F@gmail.com>
2023-03-27 17:36:16 +0300

    completed task 36

--- task36/Program.cs
+++ task36/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int CheckPosition(int[] arr)
+{
+    int tmp = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) tmp += arr[i];
+    }
+    return tmp;
+}
+
+int[] array = CreateArrayRndInt(4, -100, 100);
+PrintArray(array);
+
+int result = CheckPosition(array);
+Console.Write($" --> {result}");
