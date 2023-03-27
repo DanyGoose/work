372efc5368d8472c25971133e03b83685dfa7b29
dotnet <danila1996F@gmail.com>
2023-03-27 16:44:29 +0300

    completed task 32

--- task32/Program.cs
+++ task32/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+void ChangeArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] - arr[i] * 2;
+    }
+}
+
+int[] array = CreateArrayRndInt(12, -9, 9);
+PrintArray(array);
+
+Console.WriteLine();
+
+ChangeArray(array);
+PrintArray(array);
