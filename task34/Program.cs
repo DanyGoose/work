bd8cba7acfcd0ae6142d161925c6d3bdf7dcca4a
dotnet <danila1996F@gmail.com>
2023-03-27 17:08:08 +0300

    completed task 34

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int CheckNumber(int[] arr)
+{
+    int tmp = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) tmp++;
+    }
+    return tmp;
+}
+
+
+
+int[] array = CreateArrayRndInt(4, 100, 999);
+PrintArray(array);
+Console.WriteLine();
+
+int result = CheckNumber(array);
+Console.WriteLine($"четных чисел при этом -> {result}");
