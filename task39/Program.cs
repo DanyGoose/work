4eef1c39122763f595dfcbce8681fef0fb35adc2
dotnet <danila1996F@gmail.com>
2023-03-28 20:57:30 +0300

    completed task 39

--- task39/Program.cs
+++ task39/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Array.Reverse(array); разворачивает массив
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+void ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int tmp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = tmp;
+    }
+}
+
+int[] array = CreateArrayRndInt(5, 1, 9);
+PrintArray(array);
+
+Console.WriteLine();
+
+ReverseArray(array);
+PrintArray(array);
