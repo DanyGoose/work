2f6358b9b8a79cbec713e8bbd85c27da9ae03501
dotnet <danila1996F@gmail.com>
2023-03-28 21:39:32 +0300

    completed task 40

--- task40/Program.cs
+++ task40/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool CheckTriangle(int a, int b, int c)
+{
+    if (a < b + c
+        && b < a + c
+        && c < a + b) return true;
+    return false;
+}
+
+void PrintAnswer(bool qustion)
+{
+    if (qustion == true) Console.WriteLine("Такой треугольник возможен!");
+    else Console.WriteLine("Такого треугольника быть не может!");
+}
+
+Console.WriteLine("Введите сторону A: ");
+int sideA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону B: ");
+int sideB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону C: ");
+int sideC = Convert.ToInt32(Console.ReadLine());
+
+bool checkTriangle = CheckTriangle(sideA, sideB, sideC);
+PrintAnswer(checkTriangle);
