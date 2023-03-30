1ca8476ff174152503e33bd282c37467c1c75111
dotnet <danila1996F@gmail.com>
2023-03-31 00:31:05 +0300

    completed task 44

--- task44/Program.cs
+++ task44/Program.cs
@@ -4,7 +4,7 @@
     fibonacciArray[0] = 0; fibonacciArray[1] = 1;
     for (int i = 2; i < fibonacciArray.Length; i++)
     {
-        fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2];
+        fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
     }
     return fibonacciArray;
 }
@@ -16,23 +16,21 @@ int CheckDigitPositive(int num)
         num *= -1;
         Console.WriteLine("Ваше число было изменено на положительное!");
     }
-    if (num == 0) 
+    if (num > -2 && num < 2)
     {
-        num++;
-        Console.WriteLine("Это не подходит, прибавлена единица!");
+        num = 2;
+        Console.WriteLine("Это не подходит, заменено на двойку!");
     }
     return num;
 }
 
 void PrintArray(int[] arr)
 {
-    Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
         if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
         else Console.Write($"{arr[i]}");
     }
-    Console.Write("]");
 }
 
 Console.WriteLine("Сколько чисел фибоначчи нужно вывести? (введите целое положительное число): ");
@@ -40,4 +38,4 @@ int digit = Convert.ToInt32(Console.ReadLine());
 digit = CheckDigitPositive(digit);
 
 int[] result = FibonacciArray(digit);
-Console.WriteLine(result);
+PrintArray(result);
