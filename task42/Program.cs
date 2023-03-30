bc2380a9b0d374d2eb8ce2e4ffc7543cbf0bddf5
dotnet <danila1996F@gmail.com>
2023-03-30 23:11:04 +0300

    completed task 42

--- task42/Program.cs
+++ task42/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] TransferNumber(int num)
+{
+    int size = 1;
+    int tmp = num;
+
+    while(tmp / 2 != 0)
+    {
+        tmp = tmp / 2;
+        size++;
+    }
+
+    int[] arrTransferNumber = new int[size];
+    for (int i = 0; i < arrTransferNumber.Length; i++)
+    {
+        arrTransferNumber[i] = num % 2;
+        num = num / 2;
+    }
+
+    Array.Reverse(arrTransferNumber);
+    return arrTransferNumber;
+}
+
+void PrintArray(int[] arrTransferNumber)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrTransferNumber.Length; i++)
+    {
+        if (i < arrTransferNumber.Length - 1) Console.Write($"{arrTransferNumber[i]}, ");
+        else Console.Write($"{arrTransferNumber[i]}");
+    }
+    Console.Write("]");
+}
+
+int CheckNegativ(int num)
+{
+    if (num > -1) return num;
+    else return num /= -1; 
+}
+
+Console.WriteLine("Введите целое положительное десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = CheckNegativ(number);  
+
+int[] result = TransferNumber(number);
+PrintArray(result);
