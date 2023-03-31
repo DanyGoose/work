f9872ef8723aa774ad51f47367ffc49b5012214a
dotnet <danila1996F@gmail.com>
2023-03-31 15:22:09 +0300

    completed task 45

--- task45/Program.cs
+++ task45/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] copyArray = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        copyArray[i] = arr[i];
+    }
+    return copyArray;
+ }
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] arraySource = CreateArrayRndInt(10, -10, 10);
+PrintArray(arraySource);
+Console.WriteLine();
+int[] arrayResult = CopyArray(arraySource);
+PrintArray(arrayResult);
