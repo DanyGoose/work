47f2ea9bcd1f56c4e6d60dd4c205ae694734ad56
dotnet <danila1996F@gmail.com>
2023-04-03 21:54:33 +0300

    completed task 49

--- task47/Program.cs
+++ task47/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿double[,] CreateMatrixRndInt(int rows, int columns, double min, double max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] arrayMatrix = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(arrayMatrix);
--- task49/Program.cs
+++ task49/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] SearchForEvenIndex(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) arr[i, j] *= arr[i, j];
+        }
+    }
+    return arr;
+}
+
+int[,] arrayMatrix = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(arrayMatrix);
+
+Console.WriteLine();
+
+arrayMatrix = SearchForEvenIndex(arrayMatrix);
+PrintMatrix(arrayMatrix);
